accd90571acca2369084b692bb7cb68df371c95f
sandeepstha46 <sandeepstha64@gmail.com>
2023-03-07 13:05:43 +0545

    [Add] installed vuex

--- vue/src/App.vue
+++ vue/src/App.vue
@@ -1,8 +1,16 @@
-<script setup>
+<script>
 import HelloWorld from './components/HelloWorld.vue'
+import {mapState} from 'vuex'
+
+export default {
+  computed: {
+    ...mapState(['user'])
+  }
+}
 </script>
 
 <template>
+  <pre>{{ user }}</pre>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
